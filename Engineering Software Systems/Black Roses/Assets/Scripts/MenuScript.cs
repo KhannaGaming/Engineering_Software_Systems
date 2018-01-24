707eadce099c3c81b07f90db8117cf7cf8d0f44d
Sean Khanna <sean6beechdrive@hotmail.co.uk>
2018-01-24 16:37:53 +0000

    Update Unity

---
+++ Engineering Software Systems/Black Roses/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        PlayerPrefs.SetInt("Volume", 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+    public void pressedPlay()
+    {
+        SceneManager.LoadScene("Level01");
+    }
+
+    public void pressedSettings()
+    {
+
+    }
+
+    public void pressedQuit()
+    {
+        Application.Quit();
+    }
+
+}
