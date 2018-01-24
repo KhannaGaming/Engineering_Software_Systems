1b8e3b6de5d36ae19340d7e844655a04af8181b0
Sean Khanna <sean6beechdrive@hotmail.co.uk>
2018-01-24 20:18:10 +0000

    New submission

--- Engineering Software Systems/Black Roses/Assets/Scripts/MenuScript.cs
+++ Engineering Software Systems/Black Roses/Assets/Scripts/MenuScript.cs
@@ -2,24 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class MenuScript : MonoBehaviour
-{
+public class MenuScript : MonoBehaviour {
 
-    // Use this for initialization
-    void Start()
-    {
-        PlayerPrefs.SetInt("Volume", 10);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-        if (Input.GetKey("escape"))
+    private bool movingToSettings = false;
+    public int cameraSpeed;
+    private float dt;
+	// Use this for initialization
+	void Start () {
+        GameObject.Find("Music").GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("Volume", 1.0f);
+        dt = Time.deltaTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("escape"))
         {
             Application.Quit();
         }
+
+        if(movingToSettings == true && GameObject.Find("Moveable").GetComponent<RectTransform>().localPosition.x > -800)
+        {
+            GameObject.Find("Moveable").GetComponent<Transform>().localPosition -= new Vector3(cameraSpeed, 0, 0);
+            
+        }
+        else
+        {
+            movingToSettings = false;
+        }
+
+        PlayerPrefs.SetFloat("Volume", GameObject.Find("VolumeSlider").GetComponent<Slider>().value);
+        GameObject.Find("Music").GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("Volume", 1.0f);
     }
 
     public void pressedPlay()
@@ -29,12 +43,11 @@ public class MenuScript : MonoBehaviour
 
     public void pressedSettings()
     {
-
+        movingToSettings = true;
     }
 
     public void pressedQuit()
     {
         Application.Quit();
     }
-
 }
