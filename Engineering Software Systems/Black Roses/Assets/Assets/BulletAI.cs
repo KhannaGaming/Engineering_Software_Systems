e5476d03b483a2e1952584c0728b8d03c0145875
Sean Khanna <sean6beechdrive@hotmail.co.uk>
2018-02-06 20:11:25 +0000

    06/02/2018

---
+++ Engineering Software Systems/Black Roses/Assets/Assets/BulletAI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletAI : MonoBehaviour {
+
+
+    Rigidbody2D rb2d;
+    
+    public Vector3 mousePos;
+    public Vector3 relativePos;
+    private float bulletSpeed = 10f;
+
+    private float dt;
+    // Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        dt = Time.deltaTime;
+         mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+
+
+         relativePos = mousePos - GameObject.Find("FirePoint").transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 normPos = relativePos.normalized;
+        rb2d.velocity = normPos * bulletSpeed;
+
+        Destroy(gameObject, 2);
+
+	}
+}
---
+++ Engineering Software Systems/Black Roses/Assets/Assets/GunFiring.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunFiring : MonoBehaviour {
+
+    public Transform bulletPrefab;
+    public Transform firePointTransform;
+
+    public Vector3 mousePos;
+    public Vector3 relativePos;
+
+    public float Cooldown;
+    private float CurCooldown;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        CurCooldown += Time.deltaTime;
+        if (CurCooldown > Cooldown)
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                Instantiate(bulletPrefab, firePointTransform.position, firePointTransform.rotation);
+                CurCooldown = 0;
+            }
+        }
+	}
+
+    private void FixedUpdate()
+    {
+
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+         relativePos =  mousePos - GameObject.Find("Elbow").transform.localPosition ;
+         GameObject.Find("Elbow").transform.rotation = Quaternion.LookRotation(Vector3.forward, relativePos);
+        GameObject.Find("Elbow").transform.eulerAngles = new Vector3(0,0, GameObject.Find("Elbow").transform.eulerAngles.z+90);
+    }
+}
